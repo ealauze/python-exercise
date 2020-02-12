5f99146d6337c9b1f9045ad9e1ceccff57b80cb7
ealauze <37064420+ealauze@users.noreply.github.com>
2020-02-12 15:50:14 +0000

    commit

--- main.py
+++ main.py
@@ -4,12 +4,14 @@
 def main():
     """ Main program """
     # Code goes over here.
-    number1 = int(input("Please enter number 1 : "))
-    number2 = int(input("Please enter number 2 : "))
-    number3 = int(input("Please enter number 3 : "))
-    number4 = int(input("Please enter number 4 : "))
-    number5 = int(input("Please enter number 5 : "))
-    average = (number1+number2+number3+number4+number5)/5
+    i=1
+    numbers = 0
+    while i < 6 :
+        print("Please enter number ", i)
+        number = int(input())
+        numbers = numbers + number
+        i = i + 1
+    average = numbers / 5
     print("Average is : ", average)
     return 0
 
